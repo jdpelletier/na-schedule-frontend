5e608303f03733e30ad7304be9a60362ea92353b
jdpelletier <john.pelletier@ymail.com>
2022-06-18 09:58:37 -1000

    navbar test

--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -24,9 +24,10 @@ const NavMenu = ({page, setPage, setSubmit, setViewlog}) => {
   if(page === "nightlog"){
     return(
       <div className="navContainer">
-        <Navbar expand="lg">
+        <Navbar collapseonselect fixed='top' expand="sm">
           <Container>
-            <Navbar.Collapse id="basic-navbar-nav">
+            <Navbar.Toggle id="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="me-auto">
                 <Nav.Link onClick={() => setPage('home')}>Home</Nav.Link>
                 <Nav.Link onClick={() => setPage('links')}>Useful Links</Nav.Link>
